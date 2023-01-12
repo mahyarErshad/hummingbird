561ed05a76a1be91d7480bbe0ecc6dcc1dac9924
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-12 11:27:41 +0330

    whitespace-nowrap

--- src/Components/Header/HeaderNavLinks.jsx
+++ src/Components/Header/HeaderNavLinks.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./header.module.css";
 
-function HeaderNavLinks({ target, href, text }) {
+function HeaderNavLinks({ target, href, text, noBorder }) {
   return (
-    <a className={`${styles.borderBottom} h-full w-full inline-block py-[0.6875rem] px-[0.9375rem] cursor-pointer hover:text-primary transition-all font-medium`} href={href || "#"} target={target || ""}>
-      <span>{text}</span>
+    <a className={`max-lg:${!noBorder && styles.borderBottom} h-full w-full inline-block py-[0.6875rem] px-[0.9375rem] cursor-pointer hover:text-primary transition-all font-medium`} href={href || "#"} target={target || ""}>
+      <span className="whitespace-nowrap">{text}</span>
     </a>
   );
 }
