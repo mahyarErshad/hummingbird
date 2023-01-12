f816a2fa4f30fdb99567c6400fd3c5a80a00332e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-12 08:40:17 +0330

    Import header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Header from "./Components/Header/Header";
 
 function App() {
-  return <div className="text-red-400">hey</div>;
+  return <>
+    <Header />
+  </>;
 }
 
 export default App;
