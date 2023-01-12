1ef24b5486668e893fe91ee01e52360377b392fc
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-12 11:22:03 +0330

    HeaderNavLinks are styled

--- src/Components/Header/HeaderNavLinks.jsx
+++ src/Components/Header/HeaderNavLinks.jsx
@@ -3,7 +3,7 @@ import styles from "./header.module.css";
 
 function HeaderNavLinks({ target, href, text }) {
   return (
-    <a className={`${styles.borderBottom} h-full w-full inline-block py-[0.6875rem] px-[0.9375rem] cursor-pointer hover:opacity-90`} href={href || "#"} target={target || ""}>
+    <a className={`${styles.borderBottom} h-full w-full inline-block py-[0.6875rem] px-[0.9375rem] cursor-pointer hover:text-primary transition-all font-medium`} href={href || "#"} target={target || ""}>
       <span>{text}</span>
     </a>
   );
