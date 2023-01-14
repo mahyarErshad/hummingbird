f039ff5c0c2f45e92a3a133856bcc560c87f2a2f
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-14 12:29:50 +0330

    Removed the menu

--- src/Components/Body/Slider.jsx
+++ src/Components/Body/Slider.jsx
@@ -1,12 +1,44 @@
 import React from "react";
 
+import HeroSlider, { Overlay, Slide } from "hero-slider";
+
 import slider1 from "../../assets/images/slider/slider1.svg";
 import slider2 from "../../assets/images/slider/slider2.webp";
 import slider3 from "../../assets/images/slider/slider3.webp";
 
 function Slider() {
   return (
-<></>
+    <HeroSlider
+      height={"40vh"}
+      className="mt-20 w-[90%] mx-auto rounded-md"
+      autoplay
+      controller={{
+        initialSlide: 1,
+        slidingDuration: 500,
+        slidingDelay: 100,
+      }}
+    >
+      <Overlay></Overlay>
+
+      <Slide
+        background={{
+          backgroundImageSrc: slider1,
+          backgroundImageSizes: "contain"
+        }}
+      />
+
+      <Slide
+        background={{
+          backgroundImageSrc: slider2,
+        }}
+      />
+
+      <Slide
+        background={{
+          backgroundImageSrc: slider3,
+        }}
+      />
+    </HeroSlider>
   );
 }
 
