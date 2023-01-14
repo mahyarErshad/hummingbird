f5df4f4451ee3d5812d3fad832856dea4d588ec4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-14 08:47:54 +0330

    Imported the body

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import MainBody from "./Components/Body/MainBody";
 import Header from "./Components/Header/Header";
 
 function App() {
-  return <>
-    <Header />
-  </>;
+  return (
+    <>
+      <Header />
+      <MainBody />
+    </>
+  );
 }
 
 export default App;
