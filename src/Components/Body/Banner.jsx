ca73d04720b004b959f03768333ed52d79288e85
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-14 09:37:43 +0330

    npm i swiper

--- src/Components/Body/Banner.jsx
+++ src/Components/Body/Banner.jsx
@@ -1,59 +1,7 @@
-import HeroSlider, { Overlay, Slide, MenuNav } from "hero-slider";
+import React from "react";
 
-const bogliasco = "https://i.imgur.com/Gu5Cznz.jpg";
-const countyClare = "https://i.imgur.com/idjXzVQ.jpg";
-const craterRock = "https://i.imgur.com/8DYumaY.jpg";
-const giauPass = "https://i.imgur.com/8IuucQZ.jpg";
-
-export default function Banner() {
-  return (
-    <HeroSlider
-      height={"80vh"}
-      autoplay
-      controller={{
-        initialSlide: 1,
-        slidingDuration: 500,
-        slidingDelay: 100,
-        onSliding: (nextSlide) => console.debug("onSliding(nextSlide): ", nextSlide),
-        onBeforeSliding: (previousSlide, nextSlide) => console.debug("onBeforeSliding(previousSlide, nextSlide): ", previousSlide, nextSlide),
-        onAfterSliding: (nextSlide) => console.debug("onAfterSliding(nextSlide): ", nextSlide),
-      }}
-    >
-      <Overlay></Overlay>
-
-      <Slide
-        shouldRenderMask
-        label="Giau Pass - Italy"
-        background={{
-          backgroundImageSrc: giauPass,
-        }}
-      />
-
-      <Slide
-        shouldRenderMask
-        label="Bogliasco - Italy"
-        background={{
-          backgroundImageSrc: bogliasco,
-        }}
-      />
-
-      <Slide
-        shouldRenderMask
-        label="County Clare - Ireland"
-        background={{
-          backgroundImageSrc: countyClare,
-        }}
-      />
-
-      <Slide
-        shouldRenderMask
-        label="Crater Rock, OR - United States"
-        background={{
-          backgroundImageSrc: craterRock,
-        }}
-      />
-
-      <MenuNav />
-    </HeroSlider>
-  );
+function Banner() {
+  return <div>Banner</div>;
 }
+
+export default Banner;
