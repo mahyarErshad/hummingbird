89d51c1f283904dfce6dded787dbd499c8f78b15
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-16 12:54:12 +0330

    Body has 2 rem pb

--- src/Components/Body/MainBody.jsx
+++ src/Components/Body/MainBody.jsx
@@ -3,7 +3,7 @@ import Banner from "./Banner";
 
 function MainBody() {
   return (
-    <main className="mt-[3.75rem] max-lg:px-4 lg:px-[15%] max-md:pb-4 md:pb-8">
+    <main className="mt-[3.75rem] max-lg:px-4 lg:px-[15%] pb-8">
       <Banner />
     </main>
   );
