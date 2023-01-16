a643b5686adc7be584898257c580406ef28d9349
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-16 13:34:06 +0330

    Added the isNew ability

--- src/Components/Utils/SafeModeCard/SafeModeCard.jsx
+++ src/Components/Utils/SafeModeCard/SafeModeCard.jsx
@@ -2,12 +2,12 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
-function SafeModeCard({ text , new }) {
+function SafeModeCard({ text, isNew }) {
   return (
     <div className="flex-center py-2 px-4 border-[1px] border-[#e6e6e6] w-fit rounded-[30px] gap-2">
       <FontAwesomeIcon className="text-base font-light" icon={faCheck} />
       <span className="font-bold text-sm flex-grow">{text}</span>
-      {new && }
+      {isNew && <span className="text-[0.625rem] font-bold bg-[#ffe600] p-[0.125rem] rounded-sm">جدید</span>}
     </div>
   );
 }
