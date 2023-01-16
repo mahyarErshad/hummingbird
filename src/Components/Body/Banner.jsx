e20a3ee6a95cb893228a10a4e667e28171a63640
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-16 09:11:32 +0330

    banner-wrapper has been created

--- src/Components/Body/Banner.jsx
+++ src/Components/Body/Banner.jsx
@@ -1,9 +1,7 @@
 import React, { memo } from "react";
 
 function Banner() {
-  return <section className="banner">
-
-  </section>;
+  return <section className="banner-wrapper max-lg:w-[90%] lg:w-[65%] lg:h-[25rem]"></section>;
 }
 
 export default memo(Banner);
