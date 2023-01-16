7b41cf1f44c4b5640f6b84d4b828b36fdaa5aa69
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-16 09:17:31 +0330

    Changed banner-wrapper style

--- src/Components/Body/Banner.jsx
+++ src/Components/Body/Banner.jsx
@@ -3,8 +3,9 @@ import { ReactComponent as BannerImage } from "../../assets/images/banner.svg";
 
 function Banner() {
   return (
-    <section className="banner-wrapper max-lg:w-[90%] lg:w-[65%] lg:h-[25rem]">
+    <section className="banner-wrapper flex-center max-lg:w-[90%] lg:w-[65%] lg:h-[25rem]">
       <BannerImage />
+      <p>سایت های وردپرسی سریع تر، سبک تر و بهینه تری داشته باشید.</p>
     </section>
   );
 }
