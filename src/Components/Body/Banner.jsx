51842b5098f1755fde1155e7c2372c88f01c6d35
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-16 09:12:36 +0330

    Removed flex nowrap

--- src/Components/Body/Banner.jsx
+++ src/Components/Body/Banner.jsx
@@ -1,7 +1,9 @@
 import React, { memo } from "react";
 
 function Banner() {
-  return <section className="banner-wrapper max-lg:w-[90%] lg:w-[65%] lg:h-[25rem]"></section>;
+  return <section className="banner-wrapper max-lg:w-[90%] lg:w-[65%] lg:h-[25rem]">
+    
+  </section>;
 }
 
 export default memo(Banner);
