cba3bedcc954af3559e6522193bdaeae1bc86191
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-16 09:14:26 +0330

    Added BannerImage

--- src/Components/Body/Banner.jsx
+++ src/Components/Body/Banner.jsx
@@ -1,9 +1,12 @@
 import React, { memo } from "react";
+import { ReactComponent as BannerImage } from "../../assets/images/banner.svg";
 
 function Banner() {
-  return <section className="banner-wrapper max-lg:w-[90%] lg:w-[65%] lg:h-[25rem]">
-    
-  </section>;
+  return (
+    <section className="banner-wrapper max-lg:w-[90%] lg:w-[65%] lg:h-[25rem]">
+      <BannerImage />
+    </section>
+  );
 }
 
 export default memo(Banner);
