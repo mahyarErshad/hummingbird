1507ff7cd385d79a29855e09418949930b89ef21
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-16 09:32:11 +0330

    banner-wrapper is ready and responsive

--- src/Components/Body/Banner.jsx
+++ src/Components/Body/Banner.jsx
@@ -3,9 +3,9 @@ import { ReactComponent as BannerImage } from "../../assets/images/banner.svg";
 
 function Banner() {
   return (
-    <section className="banner-wrapper flex-center max-lg:w-[90%] lg:w-[65%] lg:h-[25rem]">
-      <BannerImage />
-      <p>سایت های وردپرسی سریع تر، سبک تر و بهینه تری داشته باشید.</p>
+    <section className="banner-wrapper flex items-center max-md:justify-center gap-4 justify-between max-lg:flex-col max-lg:w-[90%] lg:w-[70%] h-[25rem]">
+      <p className="font-bold max-md:text-3xl md:text-[2.5rem] flex-shrink-0 text-white max-lg:w-full w-[50%]">سایت های وردپرسی سریع تر، سبک تر و بهینه تری داشته باشید.</p>
+      <BannerImage className="max-md:hidden" />
     </section>
   );
 }
