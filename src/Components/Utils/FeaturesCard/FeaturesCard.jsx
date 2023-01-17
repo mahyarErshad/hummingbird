b50f4da281b4e7046dfd645d6dbfa6d96bb86a44
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 10:59:18 +0330

    Decreased the gap

--- src/Components/Utils/FeaturesCard/FeaturesCard.jsx
+++ src/Components/Utils/FeaturesCard/FeaturesCard.jsx
@@ -2,7 +2,7 @@ import React, { memo } from "react";
 
 function FeaturesCard(props) {
   return (
-    <div className="w-full max-md:mt-8 md:mt-28 px-6 py-8 border-b border-[#e6e6e6] flex items-start justify-start max-md:flex-col max-md:items-start max-md:gap-4 md:gap-72">
+    <div className="w-full max-md:mt-8 md:mt-28 px-6 py-8 border-b border-[#e6e6e6] flex items-start justify-start max-md:flex-col max-md:items-start max-md:gap-4 md:gap-48">
       <h3 className="font-bold max-md:text-[1.375rem] md:text-2xl">{props.title}</h3>
       <ul className="list-disc">
         {props.list.map((item, index) => {
