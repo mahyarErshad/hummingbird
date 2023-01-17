ad6e08930c363e0eed11eac9c073542339f6a5a5
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 09:23:31 +0330

    Changed the name pricing to features

---
+++ src/Components/Body/Features.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Features() {
+  return (
+    <section className='max-md:mt-8 md:mt-20'>
+    <h2>قیمت گذاری</h2>
+    </section>
+  )
+}
+
+export default Features
