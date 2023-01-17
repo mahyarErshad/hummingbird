99cb443ae53a30dc55a8b9b1f6ad85aabce9b005
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 11:02:47 +0330

    Changed px on large screens

--- src/Components/Body/Features.jsx
+++ src/Components/Body/Features.jsx
@@ -3,7 +3,7 @@ import FeaturesCard from "../Utils/FeaturesCard/FeaturesCard";
 
 function Features() {
   return (
-    <section className="max-md:mt-8 md:mt-12 pb-8 max-md:pt-8 md:pt-28 flex justify-start flex-col max-sm:px-8 max-lg:px-12 lg:px-[25%] items-center w-full bg-[#FAF9F5]">
+    <section className="max-md:mt-8 md:mt-12 pb-8 max-md:pt-8 md:pt-28 flex justify-start flex-col max-sm:px-8 max-lg:px-12 lg:pr-[25%] lg:pl-4 items-center w-full bg-[#FAF9F5]">
       <h2 className="font-bold max-md:text-2xl md:text-4xl">ویژگی ها</h2>
       <FeaturesCard title="تست و گزارش عملکرد" list={["نتایج دسکتاپ و موبایل", "معیارهای سایت را رصد کنید", "مشاهده معیارهای تایید شده/شکست خورده", "گزارش تجربه کاربر کروم"]} />
     </section>
