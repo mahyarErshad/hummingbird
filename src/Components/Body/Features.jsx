a0129caf69fbbc471a2daa199037c5d8b2b3c559
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 09:34:54 +0330

    h2 has style

--- src/Components/Body/Features.jsx
+++ src/Components/Body/Features.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Features() {
   return (
-    <section className="max-md:mt-8 md:mt-12 max-md:pt-8 md:pt-12 flex justify-center items-start w-full bg-[#FAF9F5]">
-      <h2>ویژگی ها</h2>
+    <section className="max-md:mt-8 md:mt-12 max-md:pt-8 md:pt-28 flex justify-center items-start w-full bg-[#FAF9F5]">
+      <h2 className="font-bold max-md:text-xl md:text-4xl">ویژگی ها</h2>
     </section>
   );
 }
