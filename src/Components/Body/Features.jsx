a6ba74ee75e3158a6522b43fe55fc3162aefa407
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 11:25:20 +0330

    Added margin bottom to h2

--- src/Components/Body/Features.jsx
+++ src/Components/Body/Features.jsx
@@ -4,7 +4,7 @@ import FeaturesCard from "../Utils/FeaturesCard/FeaturesCard";
 function Features() {
   return (
     <section className="max-md:mt-8 md:mt-12 pb-8 max-md:pt-8 md:pt-28 flex justify-start flex-col max-sm:px-8 max-lg:px-12 lg:px-[25%] items-center w-full bg-[#FAF9F5]">
-      <h2 className="font-bold max-md:text-2xl md:text-4xl">ویژگی ها</h2>
+      <h2 className="font-bold max-md:text-2xl md:text-4xl max-md:mb-8 md:mb-28">ویژگی ها</h2>
       <FeaturesCard title="تست و گزارش عملکرد" list={["نتایج دسکتاپ و موبایل", "معیارهای سایت را رصد کنید", "مشاهده معیارهای تایید شده/شکست خورده", "گزارش تجربه کاربر کروم"]} />
       <FeaturesCard title="نظارت و بهینه سازی زمان پاسخ" list={["مطمئن شوید که سایت های شما به روز هستند", "نظارت بر زمان پاسخ سرور", "تنظیم دوره زمانی گزارش"]} />
       <FeaturesCard title="تست های سرعت را اجرا کنید" list={["تست گوگل سرعت صفحه ​​را اجرا کنید", "ممیزی های PS + معیارهای عبور / شکست", "توصیه های PS را برطرف کنید", "چشم اندازهای فوری دریافت کنید"]} />
