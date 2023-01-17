30cb132a00ebdb76f87d12acefeb712e06f5e486
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 10:55:43 +0330

    Imported FeaturesCard

--- src/Components/Body/Features.jsx
+++ src/Components/Body/Features.jsx
@@ -1,10 +1,11 @@
 import React, { memo } from "react";
+import FeaturesCard from "../Utils/FeaturesCard/FeaturesCard";
 
 function Features() {
   return (
     <section className="max-md:mt-8 md:mt-12 pb-8 max-md:pt-8 md:pt-28 flex justify-start flex-col max-sm:px-8 max-lg:px-12 lg:px-[25%] items-center w-full bg-[#FAF9F5]">
       <h2 className="font-bold max-md:text-2xl md:text-4xl">ویژگی ها</h2>
-
+      <FeaturesCard />
     </section>
   );
 }
