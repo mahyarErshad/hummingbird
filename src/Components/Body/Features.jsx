9d255112c94735c7aadb025fb77303f4389d0ac6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 10:20:12 +0330

    Component is more responsive

--- src/Components/Body/Features.jsx
+++ src/Components/Body/Features.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function Features() {
   return (
-    <section className="max-md:mt-8 md:mt-12 max-md:pt-8 md:pt-28 flex justify-start flex-col items-center w-full bg-[#FAF9F5]">
-      <h2 className="font-bold max-md:text-xl md:text-4xl">ویژگی ها</h2>
-      <div className="w-full max-sm:px-8 max-md:px-12 md:px-[35%] max-md:mt-8 md:mt-28 py-14 flex items-center justify-start max-md:flex-col max-md:items-start max-md:gap-4 md:gap-44">
+    <section className="max-md:mt-8 md:mt-12 pb-8 max-md:pt-8 md:pt-28 flex justify-start flex-col max-sm:px-8 max-lg:px-12 lg:px-[25%] items-center w-full bg-[#FAF9F5]">
+      <h2 className="font-bold max-md:text-2xl md:text-4xl">ویژگی ها</h2>
+      <div className="w-full max-md:mt-8 md:mt-28 px-6 py-8 border-b border-[#e6e6e6] flex items-start justify-start max-md:flex-col max-md:items-start max-md:gap-4 md:gap-44">
         <h3 className="font-bold max-md:text-[1.375rem] md:text-2xl">عنوان</h3>
         <ul className="list-disc">
-          <li className="text-base font-medium text-[#505050]">تست</li>
+          <li className="text-base max-md:mr-8 font-medium text-[#505050]">تست</li>
           <li>تست</li>
           <li>تست</li>
           <li>تست</li>
