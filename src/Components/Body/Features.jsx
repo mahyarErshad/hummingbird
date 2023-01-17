e8460dc4530441ed57cfa9f8641d7b8e5663a04e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 10:10:38 +0330

    List is styled

--- src/Components/Body/Features.jsx
+++ src/Components/Body/Features.jsx
@@ -5,9 +5,9 @@ function Features() {
     <section className="max-md:mt-8 md:mt-12 max-md:pt-8 md:pt-28 flex justify-start flex-col items-center w-full bg-[#FAF9F5]">
       <h2 className="font-bold max-md:text-xl md:text-4xl">ویژگی ها</h2>
       <div className="w-full max-md:px-8 md:pr-[35%] max-md:mt-8 md:mt-28 py-14 flex items-center justify-start max-md:flex-col max-md:items-start max-md:gap-4 md:gap-44">
-        <h3>عنوان</h3>
-        <ul>
-          <li>تست</li>
+        <h3 className="font-bold max-md:text-[1.375rem] md:text-2xl">عنوان</h3>
+        <ul className="list-disc">
+          <li className="text-base font-medium text-[#505050]">تست</li>
           <li>تست</li>
           <li>تست</li>
           <li>تست</li>
