f2adff534a24daedf0f4dfbf24cf33fdef64f6d1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 10:22:41 +0330

    Fixed a bug

--- src/Components/Body/Features.jsx
+++ src/Components/Body/Features.jsx
@@ -4,7 +4,7 @@ function Features() {
   return (
     <section className="max-md:mt-8 md:mt-12 pb-8 max-md:pt-8 md:pt-28 flex justify-start flex-col max-sm:px-8 max-lg:px-12 lg:px-[25%] items-center w-full bg-[#FAF9F5]">
       <h2 className="font-bold max-md:text-2xl md:text-4xl">ویژگی ها</h2>
-      <div className="w-full max-md:mt-8 md:mt-28 px-6 py-8 border-b border-[#e6e6e6] flex items-start justify-start max-md:flex-col max-md:items-start max-md:gap-4 md:gap-44">
+      <div className="w-full max-md:mt-8 md:mt-28 px-6 py-8 border-b border-[#e6e6e6] flex items-start justify-start max-md:flex-col max-md:items-start max-md:gap-4 md:gap-72">
         <h3 className="font-bold max-md:text-[1.375rem] md:text-2xl">عنوان</h3>
         <ul className="list-disc">
           <li className="text-base max-md:mr-8 font-medium text-[#505050]">تست</li>
