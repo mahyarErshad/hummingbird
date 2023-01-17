b6402d49b21b00cf08faf6c1579e4ba7150e2ea2
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 09:32:48 +0330

    Moved the px here

--- src/Components/Body/Banner.jsx
+++ src/Components/Body/Banner.jsx
@@ -11,11 +11,11 @@ import SafeModeCard from "../Utils/SafeModeCard/SafeModeCard";
 
 function Banner() {
   return (
-    <>
-      <section className="banner-wrapper flex items-center max-md:justify-center gap-4 justify-between max-lg:flex-col w-full h-[25rem]">
+    <section className="max-lg:px-4 lg:px-[15%]">
+      <div className="banner-wrapper flex items-center max-md:justify-center gap-4 justify-between max-lg:flex-col w-full h-[25rem]">
         <p className="font-bold max-md:text-3xl md:text-[2.5rem] flex-shrink-0 text-white max-lg:w-full w-[50%]">سایت های وردپرسی سریع تر، سبک تر و بهینه تری داشته باشید.</p>
         <BannerImage className="max-md:hidden" />
-      </section>
+      </div>
       <div className="w-full flex-center gap-2 flex-wrap mb-24">
         <p className="text-sm whitespace-nowrap">مشتری های ما میگن</p>
         <strong className="text-base">عالی</strong>
@@ -57,7 +57,7 @@ function Banner() {
           <img src={safeMode} alt="response time" className="mx-auto" />
         </div>
       </div>
-    </>
+    </section>
   );
 }
 
