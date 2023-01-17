c263ae0e9916dcab7b184f0317cc847062c2059b
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 12:07:38 +0330

    Removed padding and giving FeaturesCard max width 960 px

--- src/Components/Utils/FeaturesCard/FeaturesCard.jsx
+++ src/Components/Utils/FeaturesCard/FeaturesCard.jsx
@@ -2,7 +2,7 @@ import React, { memo } from "react";
 
 function FeaturesCard(props) {
   return (
-    <div className="featureCards w-full px-6 py-8 border-b border-[#e6e6e6] flex items-start justify-start max-md:flex-col max-md:items-start max-md:gap-4 md:gap-8">
+    <div className="featureCards w-full max-w-[60rem] mx-auto px-6 py-8 border-b border-[#e6e6e6] flex items-start justify-start max-md:flex-col max-md:items-start max-md:gap-4 md:gap-8">
       <h3 className="font-bold max-md:text-[1.375rem] md:text-2xl max-md:w-full w-2/4">{props.title}</h3>
       <ul className="list-disc max-md:w-full w-2/4">
         {props.list.map((item, index) => {
