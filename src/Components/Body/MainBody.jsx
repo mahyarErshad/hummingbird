7e36e237093fe2f765e530e55aea721f72589023
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 09:19:36 +0330

    Imported Pricing

--- src/Components/Body/MainBody.jsx
+++ src/Components/Body/MainBody.jsx
@@ -1,10 +1,12 @@
 import React, { memo } from "react";
 import Banner from "./Banner";
+import Pricing from "./Pricing";
 
 function MainBody() {
   return (
     <main className="mt-[3.75rem] max-lg:px-4 lg:px-[15%] pb-8">
       <Banner />
+      <Pricing />
     </main>
   );
 }
