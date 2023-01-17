7d97268d6968cc80b140b1093f4f6aaa92eeca73
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 10:55:23 +0330

    FeaturesCard is dynamic

--- src/Components/Utils/FeaturesCard/FeaturesCard.jsx
+++ src/Components/Utils/FeaturesCard/FeaturesCard.jsx
@@ -1,18 +1,20 @@
-import React, { memo } from 'react'
+import React, { memo } from "react";
 
 function FeaturesCard(props) {
   return (
     <div className="w-full max-md:mt-8 md:mt-28 px-6 py-8 border-b border-[#e6e6e6] flex items-start justify-start max-md:flex-col max-md:items-start max-md:gap-4 md:gap-72">
-    <h3 className="font-bold max-md:text-[1.375rem] md:text-2xl">عنوان</h3>
-    <ul className="list-disc">
-      <li className="text-base max-md:mr-8 font-medium text-[#505050]">تست</li>
-      <li>تست</li>
-      <li>تست</li>
-      <li>تست</li>
-      <li>تست</li>
-    </ul>
-  </div>
-  )
+      <h3 className="font-bold max-md:text-[1.375rem] md:text-2xl">{props.title}</h3>
+      <ul className="list-disc">
+        {props.list.map((item, index) => {
+          return (
+            <li key={index} className="text-base max-md:mr-8 font-medium text-[#505050]">
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export default memo(FeaturesCard)
+export default memo(FeaturesCard);
