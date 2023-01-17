82ca3396027facde96ddb1290ee3bfe1954b2182
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 10:58:12 +0330

    Passed props to FeaturesCard

--- src/Components/Body/Features.jsx
+++ src/Components/Body/Features.jsx
@@ -5,7 +5,7 @@ function Features() {
   return (
     <section className="max-md:mt-8 md:mt-12 pb-8 max-md:pt-8 md:pt-28 flex justify-start flex-col max-sm:px-8 max-lg:px-12 lg:px-[25%] items-center w-full bg-[#FAF9F5]">
       <h2 className="font-bold max-md:text-2xl md:text-4xl">ویژگی ها</h2>
-      <FeaturesCard />
+      <FeaturesCard title="تست و گزارش عملکرد" list={["نتایج دسکتاپ و موبایل", "معیارهای سایت را رصد کنید", "مشاهده معیارهای تایید شده/شکست خورده", "گزارش تجربه کاربر کروم"]} />
     </section>
   );
 }
