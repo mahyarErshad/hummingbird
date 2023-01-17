04f242e5bcc88d69e79c15078f9e21ba0b878206
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 09:54:12 +0330

    List is responsive

--- src/Components/Body/Features.jsx
+++ src/Components/Body/Features.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 function Features() {
   return (
-    <section className="max-md:mt-8 md:mt-12 max-md:pt-8 md:pt-28 flex justify-center items-start w-full bg-[#FAF9F5]">
+    <section className="max-md:mt-8 md:mt-12 max-md:pt-8 md:pt-28 flex justify-start flex-col items-center w-full bg-[#FAF9F5]">
       <h2 className="font-bold max-md:text-xl md:text-4xl">ویژگی ها</h2>
+      <div className="max-md:px-8 md:pr-[35%] max-md:mt-8 md:mt-28 flex items-center justify-start max-md:flex-col max-md:items-start max-md:gap-4 md:gap-44 w-full">
+      <h3>عنوان</h3>
+      <ul>
+        <li>تست</li>
+        <li>تست</li>
+        <li>تست</li>
+        <li>تست</li>
+        <li>تست</li>
+      </ul>
+      </div>
     </section>
   );
 }
