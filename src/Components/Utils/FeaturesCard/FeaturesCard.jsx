c72ec1d99bf8a72f4e57144890febe3bc8d346f6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 10:24:00 +0330

    Using memo

--- src/Components/Utils/FeaturesCard/FeaturesCard.jsx
+++ src/Components/Utils/FeaturesCard/FeaturesCard.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { memo } from 'react'
 
-function FeaturesCard() {
+function FeaturesCard(props) {
   return (
     <div className="w-full max-md:mt-8 md:mt-28 px-6 py-8 border-b border-[#e6e6e6] flex items-start justify-start max-md:flex-col max-md:items-start max-md:gap-4 md:gap-72">
     <h3 className="font-bold max-md:text-[1.375rem] md:text-2xl">عنوان</h3>
@@ -15,4 +15,4 @@ function FeaturesCard() {
   )
 }
 
-export default FeaturesCard
+export default memo(FeaturesCard)
