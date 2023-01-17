e2329efd55d8b79814a9cccef7f0adf30be1bfe9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 11:20:04 +0330

    FeaturesCard is fully responsive

--- src/Components/Body/Features.jsx
+++ src/Components/Body/Features.jsx
@@ -3,12 +3,10 @@ import FeaturesCard from "../Utils/FeaturesCard/FeaturesCard";
 
 function Features() {
   return (
-    <section className="max-md:mt-8 md:mt-12 pb-8 max-md:pt-8 md:pt-28 flex justify-start flex-col items-center w-full bg-[#FAF9F5]">
-      <h2 className="font-bold max-md:text-3xl md:text-4xl">ویژگی ها</h2>
-      <div className="max-sm:px-8 max-lg:px-12 lg:pr-[25%] lg:pl-4 self-start">
-        <FeaturesCard title="تست و گزارش عملکرد" list={["نتایج دسکتاپ و موبایل", "معیارهای سایت را رصد کنید", "مشاهده معیارهای تایید شده/شکست خورده", "گزارش تجربه کاربر کروم"]} />
-        <FeaturesCard title="نظارت و بهینه سازی زمان پاسخ" list={["مطمئن شوید که سایت های شما به روز هستند", "نظارت بر زمان پاسخ سرور", "تنظیم دوره زمانی گزارش"]} />
-      </div>
+    <section className="max-md:mt-8 md:mt-12 pb-8 max-md:pt-8 md:pt-28 flex justify-start flex-col max-sm:px-8 max-lg:px-12 lg:px-[25%] items-center w-full bg-[#FAF9F5]">
+      <h2 className="font-bold max-md:text-2xl md:text-4xl">ویژگی ها</h2>
+      <FeaturesCard title="تست و گزارش عملکرد" list={["نتایج دسکتاپ و موبایل", "معیارهای سایت را رصد کنید", "مشاهده معیارهای تایید شده/شکست خورده", "گزارش تجربه کاربر کروم"]} />
+      <FeaturesCard title="نظارت و بهینه سازی زمان پاسخ" list={["مطمئن شوید که سایت های شما به روز هستند", "نظارت بر زمان پاسخ سرور", "تنظیم دوره زمانی گزارش"]} />
     </section>
   );
 }
