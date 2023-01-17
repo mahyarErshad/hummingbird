ced5338d4a6e087df450d20967b7ebd59e9c9445
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-17 09:31:01 +0330

    Fixed a bug

--- src/Components/Body/Features.jsx
+++ src/Components/Body/Features.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Features() {
   return (
-    <section className='max-md:mt-8 md:mt-20'>
-    <h2>قیمت گذاری</h2>
+    <section className="max-md:mt-8 md:mt-12 max-md:pt-8 md:pt-12 flex justify-center items-start w-full bg-[#FAF9F5]">
+      <h2>ویژگی ها</h2>
     </section>
-  )
+  );
 }
 
-export default Features
+export default Features;
