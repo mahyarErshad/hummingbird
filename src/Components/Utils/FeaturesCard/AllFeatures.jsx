ef04cef5dc0835a88cd2a74d15e2c5f66aaba052
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-19 09:44:40 +0330

    Created an array for generating cards

--- src/Components/Utils/FeaturesCard/AllFeatures.jsx
+++ src/Components/Utils/FeaturesCard/AllFeatures.jsx
@@ -4,6 +4,7 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function AllFeatures() {
+  const data = ["ارائه گزارش عملکرد", "توصیه های بهبود سرعت"];
   return (
     <div className={`${styles.wrapper} pt-14 w-full max-w-[60rem] mx-auto px-6 flex items-start justify-start flex-wrap max-md:items-start max-md:gap-4 md:gap-8`}>
       <h2 className="font-bold max-md:text-[1.375rem] md:text-2xl w-full mb-14">تمام ویژگی ها</h2>
