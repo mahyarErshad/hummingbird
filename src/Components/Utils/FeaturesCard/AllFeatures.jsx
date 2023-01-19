04c5c91ff95eef1d3052c38cca5558130740f15a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-19 09:12:52 +0330

    Added the wrapper and h2

--- src/Components/Utils/FeaturesCard/AllFeatures.jsx
+++ src/Components/Utils/FeaturesCard/AllFeatures.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function AllFeatures() {
   return (
-    <div>AllFeatures</div>
-  )
+    <div className="pt-14 w-full max-w-[60rem] mx-auto px-6 flex items-start justify-start max-md:flex-col max-md:items-start max-md:gap-4 md:gap-8">
+      <h2 className="font-bold max-md:text-[1.375rem] md:text-2xl max-md:w-full w-2/4">تمام ویژگی ها</h2>
+    </div>
+  );
 }
 
-export default AllFeatures
+export default AllFeatures;
