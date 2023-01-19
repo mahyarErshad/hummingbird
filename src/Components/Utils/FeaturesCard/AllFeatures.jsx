0baaded226770ee6511e262e5893cebf20dc23f4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-19 10:11:39 +0330

    Looping into array to create cards

--- src/Components/Utils/FeaturesCard/AllFeatures.jsx
+++ src/Components/Utils/FeaturesCard/AllFeatures.jsx
@@ -5,12 +5,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function AllFeatures() {
   const data = ["ارائه گزارش عملکرد", "توصیه های بهبود سرعت"];
+
   return (
-    <div className={`${styles.wrapper} pt-14 w-full max-w-[60rem] mx-auto px-6 flex items-start justify-start flex-wrap max-md:items-start max-md:gap-4 md:gap-8`}>
+    <div className={`${styles.wrapper} pt-14 w-full max-w-[60rem] mx-auto px-6 flex flex-nowrap items-start justify-start max-md:items-start`}>
       <h2 className="font-bold max-md:text-[1.375rem] md:text-2xl w-full mb-14">تمام ویژگی ها</h2>
       {data.map((item, index) => {
         return (
-          <div key={index} className={`${styles.card} max-md:w-full md:w-2/4 md:max-w-[26.25rem]`}>
+          <div key={index} className={`${styles.card} max-md:w-full my-3 md:w-[26.25rem]`}>
             <FontAwesomeIcon className={`${styles.icon} w-fit`} icon={faCheck} />
             <p className="whitespace-nowrap font-medium flex-grow">{item}</p>
           </div>
