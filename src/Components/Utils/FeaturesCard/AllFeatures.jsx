5b5a51eb896f42a8eba1c66c65ae3b3944d15ae0
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-19 09:42:21 +0330

    Component is responsive

--- src/Components/Utils/FeaturesCard/AllFeatures.jsx
+++ src/Components/Utils/FeaturesCard/AllFeatures.jsx
@@ -8,8 +8,8 @@ function AllFeatures() {
     <div className={`${styles.wrapper} pt-14 w-full max-w-[60rem] mx-auto px-6 flex items-start justify-start flex-wrap max-md:items-start max-md:gap-4 md:gap-8`}>
       <h2 className="font-bold max-md:text-[1.375rem] md:text-2xl w-full mb-14">تمام ویژگی ها</h2>
       <div className={`${styles.card} max-md:w-full md:w-2/4`}>
-        <FontAwesomeIcon className={`${styles.icon} max-md:w-full md:w-2/4 `} icon={faCheck} />
-        <p className="whitespace-nowrap font-medium">ارائه گزارش عملکرد</p>
+        <FontAwesomeIcon className={`${styles.icon} w-fit`} icon={faCheck} />
+        <p className="whitespace-nowrap font-medium flex-grow">ارائه گزارش عملکرد</p>
       </div>
     </div>
   );
