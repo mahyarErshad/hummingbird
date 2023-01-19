5675ca5a9fd999d384a98c3bbf26c89f38100bd7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-19 09:47:50 +0330

    Cards are responsive

--- src/Components/Utils/FeaturesCard/AllFeatures.jsx
+++ src/Components/Utils/FeaturesCard/AllFeatures.jsx
@@ -8,10 +8,14 @@ function AllFeatures() {
   return (
     <div className={`${styles.wrapper} pt-14 w-full max-w-[60rem] mx-auto px-6 flex items-start justify-start flex-wrap max-md:items-start max-md:gap-4 md:gap-8`}>
       <h2 className="font-bold max-md:text-[1.375rem] md:text-2xl w-full mb-14">تمام ویژگی ها</h2>
-      <div className={`${styles.card} max-md:w-full md:w-2/4`}>
-        <FontAwesomeIcon className={`${styles.icon} w-fit`} icon={faCheck} />
-        <p className="whitespace-nowrap font-medium flex-grow">ارائه گزارش عملکرد</p>
-      </div>
+      {data.map((item, index) => {
+        return (
+          <div key={index} className={`${styles.card} max-md:w-full md:w-2/4 md:max-w-[26.25rem]`}>
+            <FontAwesomeIcon className={`${styles.icon} w-fit`} icon={faCheck} />
+            <p className="whitespace-nowrap font-medium flex-grow">{item}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
