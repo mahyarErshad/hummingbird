19f070407f86a7b825ff18003b4be08a2c0f840d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-21 09:58:18 +0330

    A div is added and styled

--- src/Components/Body/Badges.jsx
+++ src/Components/Body/Badges.jsx
@@ -6,6 +6,9 @@ function Badges() {
       <h2 className="font-bold max-md:text-xl md:text-4xl mb-4">توصیه شده توسط</h2>
       <h3 className="font-bold max-md:text-xl md:text-4xl mb-8">توسعه دهندگان وردپرس</h3>
       <p className="text-center font-medium text-lg max-w-[37.5rem] leading-8 max-md:mb-14 md:mb-20">توسعه دهندگان وردپرس در سر تا سر دنیا WPMU DEV را ترجیح میدهند. با میانگین امتیاز 4.9 از 5، بیش از 5000 امتیاز 5/5 و برنده سیزدهمین جشنواره بهترین محصول G2 در بخش بهترین محصول.</p>
+      <div className="flex items-start justify-between min-w-[55rem] max-w-[62.5rem]">
+
+      </div>
     </section>
   );
 }
