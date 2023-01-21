d35e7fee47595b286f8df35329e3d98b62913ff7
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-21 10:07:58 +0330

    Styles are done

--- src/Components/Body/Badges.jsx
+++ src/Components/Body/Badges.jsx
@@ -9,9 +9,9 @@ function Badges() {
       <p className="text-center font-medium text-lg max-w-[37.5rem] leading-8 max-md:mb-14 md:mb-20">توسعه دهندگان وردپرس در سر تا سر دنیا WPMU DEV را ترجیح میدهند. با میانگین امتیاز 4.9 از 5، بیش از 5000 امتیاز 5/5 و برنده سیزدهمین جشنواره بهترین محصول G2 در بخش بهترین محصول.</p>
       <div className="flex items-start justify-between min-w-[55rem] max-w-[62.5rem]">
         <div className="flex-center max-w-[16.25rem] gap-1">
+          <Wheat className="flip" />
+          <p className="max-w-[6.125rem] font-bold text-sm text-center">انتخاب توسعه دهندگان وب</p>
           <Wheat />
-          <p className="max-w-[6.125rem] font-bold text-sm">انتخاب توسعه دهندگان وب</p>
-          <Wheat className="" />
         </div>
       </div>
     </section>
