8a22644ff8e7047acb0f9285a033bf811f031c11
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-21 09:26:41 +0330

    Section has 1 rem px

--- src/Components/Body/Badges.jsx
+++ src/Components/Body/Badges.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Badges() {
   return (
-    <section className="flex-center flex-col pb-8">
+    <section className="flex-center flex-col pb-8 px-4">
       <h2 className="font-bold max-md:text-xl md:text-4xl mb-4">توصیه شده توسط</h2>
       <h3 className="font-bold max-md:text-xl md:text-4xl mb-8">توسعه دهندگان وردپرس</h3>
       <p className="text-center font-medium text-lg max-w-[37.5rem] leading-8">توسعه دهندگان وردپرس در سر تا سر دنیا WPMU DEV را ترجیح میدهند. با میانگین امتیاز 4.9 از 5، بیش از 5000 امتیاز 5/5 و برنده سیزدهمین جشنواره بهترین محصول G2 در بخش بهترین محصول.</p>
