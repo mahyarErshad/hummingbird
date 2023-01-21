ac4ee09ecb38303653ad748795565c8050d9c696
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-21 09:26:12 +0330

    P tag has styles

--- src/Components/Body/Badges.jsx
+++ src/Components/Body/Badges.jsx
@@ -4,8 +4,8 @@ function Badges() {
   return (
     <section className="flex-center flex-col">
       <h2 className="font-bold max-md:text-xl md:text-4xl mb-4">توصیه شده توسط</h2>
-      <h3 className="font-bold max-md:text-xl md:text-4xl mb-5">توسعه دهندگان وردپرس</h3>
-      <p>توسعه دهندگان وردپرس در سر تا سر دنیا WPMU DEV را ترجیح میدهند. با میانگین امتیاز 4.9 از 5، بیش از 5000 امتیاز 5/5 و برنده سیزدهمین جشنواره بهترین محصول G2 در بخش بهترین محصول.</p>
+      <h3 className="font-bold max-md:text-xl md:text-4xl mb-8">توسعه دهندگان وردپرس</h3>
+      <p className="text-center font-medium text-lg max-w-[37.5rem] leading-8">توسعه دهندگان وردپرس در سر تا سر دنیا WPMU DEV را ترجیح میدهند. با میانگین امتیاز 4.9 از 5، بیش از 5000 امتیاز 5/5 و برنده سیزدهمین جشنواره بهترین محصول G2 در بخش بهترین محصول.</p>
     </section>
   );
 }
