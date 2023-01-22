9d110a1ec1059a92d161af89dfc3f4080c09b21d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-22 09:51:19 +0330

    Uninstall react-alice-carousel

--- src/Components/Utils/Carousel/Carousel.jsx
+++ src/Components/Utils/Carousel/Carousel.jsx
@@ -1,31 +1,9 @@
-import React from "react";
-import AliceCarousel from "react-alice-carousel";
-import "react-alice-carousel/lib/scss/alice-carousel.scss";
+import React from 'react'
 
-const responsive = {
-  0: { items: 1 },
-  568: { items: 2 },
-  1024: { items: 3 },
-};
-
-const items = [
-  <div className="item" data-value="1">
-    1
-  </div>,
-  <div className="item" data-value="2">
-    2
-  </div>,
-  <div className="item" data-value="3">
-    3
-  </div>,
-  <div className="item" data-value="4">
-    4
-  </div>,
-  <div className="item" data-value="5">
-    5
-  </div>,
-];
-
-export default function Carousel() {
-  return <AliceCarousel mouseTracking items={items} responsive={responsive} controlsStrategy="alternate" />;
+function Carousel() {
+  return (
+    <div>Carousel</div>
+  )
 }
+
+export default Carousel
