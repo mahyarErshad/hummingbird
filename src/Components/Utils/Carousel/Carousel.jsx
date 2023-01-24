5173591cfced0f815985edda5f8e773e2f8e7069
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-24 11:44:28 +0330

    Removed stylesheet link

--- src/Components/Utils/Carousel/Carousel.jsx
+++ src/Components/Utils/Carousel/Carousel.jsx
@@ -1,60 +1,22 @@
 import React from "react";
+import "./CarouselStyles.css";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import Slider from "react-slick";
 
-function Carousel() {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-  const settings = {
-    dots: false,
-    autoplay: true,
-    draggable: true,
-    swipeToSlide: true,
-    accessibility: true,
-    infinite: false,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    cssEase: "linear",
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 1400,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          dots: false,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
+export default function App() {
+  const renderSlides = () =>
+    [1, 2, 3, 4, 5, 6, 7, 8].map((num) => (
+      <div>
+        <h3>Slide {num}</h3>
+      </div>
+    ));
+
   return (
-    <>
-      <Slider {...settings} centerMode={true} centerPadding="20px" rtl={true} className="bg-red-700">
-        {arr.map((item, index) => (
-          <h1>hi</h1>
-        ))}
-      </Slider>
-    </>
+    <div className="wrapper">
+      <Slider dots={true}>{renderSlides()}</Slider>
+    </div>
   );
 }
-
-export default Carousel;
