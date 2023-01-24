229078e76f7a496b334b1998a5030df6d49f1727
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-24 12:14:34 +0330

    Imported slick-carousel's styles

--- src/Components/Utils/Carousel/Carousel.jsx
+++ src/Components/Utils/Carousel/Carousel.jsx
@@ -1,22 +1,60 @@
 import React from "react";
-import "./CarouselStyles.css";
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
 import Slider from "react-slick";
 
-export default function App() {
-  const renderSlides = () =>
-    [1, 2, 3, 4, 5, 6, 7, 8].map((num) => (
-      <div>
-        <h3>Slide {num}</h3>
-      </div>
-    ));
-
+function Carousel() {
+  const productCarousel = {
+    dots: false,
+    autoplay: true,
+    draggable: true,
+    swipeToSlide: true,
+    accessibility: true,
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    cssEase: "linear",
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          dots: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
-    <div className="wrapper">
-      <Slider dots={true}>{renderSlides()}</Slider>
+    <div className="w-full">
+      {/* <a className='d-none d-lg-block text-start'>مشاهده همه</a> */}
+      <Slider {...productCarousel} centerMode={true} centerPadding="20px" rtl={true} className="py-2">
+        {[1, 2, 3, 4, 5, 6, 7].map((item, index) => (
+          <h1>{index}</h1>
+        ))}
+      </Slider>
     </div>
   );
 }
+
+export default Carousel;
