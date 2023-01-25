299d12fa69233bbc4be89f2c7b6ca3e7eb2434c4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-25 08:37:06 +0330

    Container has been defined

--- src/Components/Utils/Carousel/Carousel.jsx
+++ src/Components/Utils/Carousel/Carousel.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { ReactComponent as Wheat } from "../../../assets/images/wheat.svg";
 
 function Carousel() {
   return (
-    <div>Carousel</div>
-  )
+    <div className="max-w-[58.75rem] mx-auto flex-center gap-5 mt-20">
+      <button>
+        <Wheat className="flip" fill="#1B6169" />
+      </button>
+      <button>
+        <Wheat fill="#1B6169" />
+      </button>
+    </div>
+  );
 }
 
-export default Carousel
+export default Carousel;
