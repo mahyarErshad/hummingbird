0a0c8d450f2caab0cc3461ec21dcd54853a667f4
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-28 09:59:28 +0330

    FAQCard needs a title by props

--- src/Components/Body/FAQ.jsx
+++ src/Components/Body/FAQ.jsx
@@ -6,7 +6,7 @@ function FAQ() {
     <section className="py-24 md:px-20 max-md:px-4 w-[95%] max-w-[60rem] mx-auto bg-[#fafafa] rounded-[20px] flex-center flex-col mb-12">
       <h2 className="text-center font-bold max-md:text-2xl md:text-4xl">سوالات متداول</h2>
       <p className="mt-8 mr-8 mb-3 ml-0 text-[#505050] text-[0.8125rem] font-medium self-start">بهینه سازی صفحه</p>
-      <FAQCard />
+      <FAQCard title="اگر قبلاً یک افزونه کش مانند WP Rocket یا W3 Total Cache داشته باشم به Hummingbird نیاز دارم؟ اگر سرویس کش قبلاً توسط میزبانی من ارائه شده باشد چه می شود؟" />
     </section>
   );
 }
