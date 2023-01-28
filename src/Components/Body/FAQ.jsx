ebf0483e20ab5ff9cb30b35b8942613027bd7812
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-28 09:47:53 +0330

    Removed padding bottom

--- src/Components/Body/FAQ.jsx
+++ src/Components/Body/FAQ.jsx
@@ -9,7 +9,7 @@ function FAQ() {
       <h2 className="text-center font-bold max-md:text-2xl md:text-4xl">سوالات متداول</h2>
       <p className="mt-8 mr-8 mb-3 ml-0 text-[#505050] text-[0.8125rem] font-medium self-start">بهینه سازی صفحه</p>
       <div onClick={() => setOpen((prev) => !prev)} className="border-t border-b border-[#e6e6e6] w-full py-3 px-8 cursor-pointer flex-center transition-all duration-300 hover:border-primary flex-col">
-        <div className="flex-center pb-8">
+        <div className="flex-center">
           <h3 className="font-medium text-lg flex-grow">اگر قبلاً یک افزونه کش مانند WP Rocket یا W3 Total Cache داشته باشم به Hummingbird نیاز دارم؟ اگر سرویس کش قبلاً توسط میزبانی من ارائه شده باشد چه می شود؟</h3>
           <FontAwesomeIcon icon={faPlus} className="w-[0.625rem]" />
         </div>
