f5e6047f9502450aee19e21a9399cd54ead0026d
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-28 11:10:03 +0330

    Title gets bold when active

--- src/Components/Utils/FAQCard/FAQCard.jsx
+++ src/Components/Utils/FAQCard/FAQCard.jsx
@@ -7,7 +7,7 @@ function FAQCard({ title, description }) {
   return (
     <figure onClick={() => setOpen((prev) => !prev)} className="border-t border-b border-[#e6e6e6] w-full py-3 px-8 cursor-pointer flex transition-all duration-300 hover:border-primary hover:text-primary flex-col">
       <div className="flex justify-start items-center text-inherit">
-        <h3 className="font-medium text-lg flex-grow text-black">{title}</h3>
+        <h3 className={`text-lg flex-grow text-black ${open ? "font-bold" : "font-medium"}`}>{title}</h3>
         <FontAwesomeIcon icon={open ? faMinus : faPlus} className="w-[0.625rem]" />
       </div>
       <p className={`text-[0.9375rem] font-medium text-[#505050] ${open ? "FaqOpen" : "FaqClose"}`}>{description}</p>
