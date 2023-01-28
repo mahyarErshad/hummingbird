2abd4f37270fa9f932de5b9b2be21ba20ab1ff46
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-28 09:02:17 +0330

    Width is 95% by a max width of 60rem

--- src/Components/Body/FAQ.jsx
+++ src/Components/Body/FAQ.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function FAQ() {
   return (
-    <section className='py-24 px-20 max-w-[60rem] mx-auto bg-[#fafafa] rounded-[20px] flex-center'>
-    <h2 className='text-center font-bold max-md:text-2xl md:text-4xl'>سوالات متداول</h2>
+    <section className="py-24 px-20 w-[95%] max-w-[60rem] mx-auto bg-[#fafafa] rounded-[20px] flex-center">
+      <h2 className="text-center font-bold max-md:text-2xl md:text-4xl">سوالات متداول</h2>
     </section>
-  )
+  );
 }
 
-export default FAQ
+export default FAQ;
