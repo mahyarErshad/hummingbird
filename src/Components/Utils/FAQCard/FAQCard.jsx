3d3ce770079861dc509f956ff8fb1fbe42d264a1
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-28 10:04:32 +0330

    icon is changing color on hover

--- src/Components/Utils/FAQCard/FAQCard.jsx
+++ src/Components/Utils/FAQCard/FAQCard.jsx
@@ -6,9 +6,9 @@ function FAQCard({ title, description }) {
   const [open, setOpen] = useState(false);
   return (
     <figure>
-      <div onClick={() => setOpen((prev) => !prev)} className="border-t border-b border-[#e6e6e6] w-full py-3 px-8 cursor-pointer flex-center transition-all duration-300 hover:border-primary flex-col">
-        <div className="flex-center">
-          <h3 className="font-medium text-lg flex-grow">{title}</h3>
+      <div onClick={() => setOpen((prev) => !prev)} className="border-t border-b border-[#e6e6e6] w-full py-3 px-8 cursor-pointer flex-center transition-all duration-300 hover:border-primary hover:text-primary flex-col">
+        <div className="flex-center text-inherit">
+          <h3 className="font-medium text-lg flex-grow text-black">{title}</h3>
           <FontAwesomeIcon icon={open ? faMinus : faPlus} className="w-[0.625rem]" />
         </div>
         <p className={`text-[0.9375rem] font-medium text-[#505050] ${open ? "FaqOpen" : "FaqClose"}`}>{description}</p>
