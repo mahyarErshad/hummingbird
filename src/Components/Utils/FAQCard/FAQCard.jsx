d999b05d43979c80ad0c2e85e69e9000cfe1fcce
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-28 13:19:22 +0330

    Fixed a bug

--- src/Components/Utils/FAQCard/FAQCard.jsx
+++ src/Components/Utils/FAQCard/FAQCard.jsx
@@ -6,9 +6,9 @@ function FAQCard({ title, description }) {
   const [open, setOpen] = useState(false);
   return (
     <figure onClick={() => setOpen((prev) => !prev)} className="border-t border-b border-[#e6e6e6] w-full py-3 px-8 cursor-pointer flex transition-all duration-300 hover:border-primary hover:text-primary flex-col">
-      <div className="flex justify-start items-center text-inherit">
-        <h3 className={`text-lg flex-grow text-black ${open ? "font-bold" : "font-medium"}`}>{title}</h3>
+      <div className="flex justify-start items-center text-inherit gap-2">
         <FontAwesomeIcon icon={open ? faMinus : faPlus} className="w-[0.625rem]" />
+        <h3 className={`text-lg flex-grow text-black ${open ? "font-bold" : "font-medium"}`}>{title}</h3>
       </div>
       <p className={`text-[0.9375rem] font-medium text-[#505050] ${open ? "FaqOpen" : "FaqClose"}`}>{description}</p>
     </figure>
