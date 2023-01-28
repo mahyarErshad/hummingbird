fe9d75a23c5adf62576d22fd919231ed5009ce6a
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-28 10:36:54 +0330

    Fixed text direction

--- src/Components/Utils/FAQCard/FAQCard.jsx
+++ src/Components/Utils/FAQCard/FAQCard.jsx
@@ -5,14 +5,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function FAQCard({ title, description }) {
   const [open, setOpen] = useState(false);
   return (
-    <figure>
-      <div onClick={() => setOpen((prev) => !prev)} className="border-t border-b border-[#e6e6e6] w-full py-3 px-8 cursor-pointer flex-center transition-all duration-300 hover:border-primary hover:text-primary flex-col">
-        <div className="flex-center text-inherit">
-          <h3 className="font-medium text-lg flex-grow text-black">{title}</h3>
-          <FontAwesomeIcon icon={open ? faMinus : faPlus} className="w-[0.625rem]" />
-        </div>
-        <p className={`text-[0.9375rem] font-medium text-[#505050] ${open ? "FaqOpen" : "FaqClose"}`}>{description}</p>
+    <figure onClick={() => setOpen((prev) => !prev)} className="border-t border-b border-[#e6e6e6] w-full py-3 px-8 cursor-pointer flex transition-all duration-300 hover:border-primary hover:text-primary flex-col">
+      <div className="flex justify-start items-center text-inherit">
+        <h3 className="font-medium text-lg flex-grow text-black">{title}</h3>
+        <FontAwesomeIcon icon={open ? faMinus : faPlus} className="w-[0.625rem]" />
       </div>
+      <p className={`text-[0.9375rem] font-medium text-[#505050] ${open ? "FaqOpen" : "FaqClose"}`}>{description}</p>
     </figure>
   );
 }
