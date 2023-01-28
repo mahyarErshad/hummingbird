eb34c48c57388d043fb400f4f53e247b63cd9546
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-28 09:17:40 +0330

    a div is created

--- src/Components/Body/FAQ.jsx
+++ src/Components/Body/FAQ.jsx
@@ -4,7 +4,10 @@ function FAQ() {
   return (
     <section className="py-24 px-20 w-[95%] max-w-[60rem] mx-auto bg-[#fafafa] rounded-[20px] flex-center flex-col">
       <h2 className="text-center font-bold max-md:text-2xl md:text-4xl">سوالات متداول</h2>
-      <p className="mt-8 mr-8 mb-3 ml-0 text-[#505050] text-[0.8125rem] font-medium">بهینه سازی صفحه</p>
+      <p className="mt-8 mr-8 mb-3 ml-0 text-[#505050] text-[0.8125rem] font-medium self-start">بهینه سازی صفحه</p>
+      <div className="border-t border-b border-[#e6e6e6] w-full py-3 px-8 cursor-pointer flex-center">
+
+      </div>
     </section>
   );
 }
