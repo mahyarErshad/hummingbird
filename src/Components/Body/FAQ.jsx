3f78218c734013e8908d1faa3d5ad0ab7f4082d6
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-28 09:09:36 +0330

    Flex direction is col

--- src/Components/Body/FAQ.jsx
+++ src/Components/Body/FAQ.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function FAQ() {
   return (
-    <section className="py-24 px-20 w-[95%] max-w-[60rem] mx-auto bg-[#fafafa] rounded-[20px] flex-center">
+    <section className="py-24 px-20 w-[95%] max-w-[60rem] mx-auto bg-[#fafafa] rounded-[20px] flex-center flex-col">
       <h2 className="text-center font-bold max-md:text-2xl md:text-4xl">سوالات متداول</h2>
       <p className="mt-8 mr-8 mb-3 ml-0 text-[#505050] text-[0.8125rem] font-medium">بهینه سازی صفحه</p>
     </section>
