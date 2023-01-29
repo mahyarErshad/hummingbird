03c7dd9100dc40c05a914bc8ee15b4897038932c
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-29 09:52:03 +0330

    Footer is done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import MyButton from "../Utils/MyButton/MyButton";
 
 function Footer() {
   return (
-    <footer className="mt-14 w-full bg-[#FAF8F0] py-12 px-8 flex-center flex-wrap max-md:gap-8 md:gap-16">
-      <MyButton onClick={() => window.open("https://www.zhaket.com/web/hummingbird-pro/support", "_blank")} className="max-sm:w-full" text="ارتباط با پشتیبانی" />
-      <MyButton onClick={() => window.open("https://www.zhaket.com/web/hummingbird-pro", "_blank")} className="max-sm:w-full" text="خرید نسخه حرفه ای" />
+    <footer className="mt-14 w-full bg-[#FAF8F0] py-12 px-8 flex-center flex-wrap max-md:gap-4 md:gap-16">
+      <MyButton onClick={() => window.open("https://www.zhaket.com/web/hummingbird-pro", "_blank")} className="bg-primary" text="خرید نسخه حرفه ای" />
+      <MyButton onClick={() => window.open("https://www.zhaket.com/web/hummingbird-pro/support", "_blank")} className="bg-[#1b6169]" text="ارتباط با پشتیبانی" />
     </footer>
   );
 }
