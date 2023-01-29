fbd08121c0f45e9708ab19c2878de37cf26afb36
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-29 09:25:21 +0330

    Button is initialized

--- src/Components/Utils/MyButton/MyButton.jsx
+++ src/Components/Utils/MyButton/MyButton.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-function MyButton({ text, onClick,className }) {
-  return <button className={`${className}`} onClick={() => onClick}>{text}</button>;
+function MyButton({ text, onClick, className }) {
+  return (
+    <button className={`${className}`} onClick={() => onClick}>
+      {text}
+    </button>
+  );
 }
 
 export default MyButton;
