fa823795e94bef70811473f3ac9ef83c4fe39b57
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-29 09:26:22 +0330

    Imported 2 buttons

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import MyButton from '../Utils/MyButton/MyButton'
 
 function Footer() {
   return (
-    <footer className='mt-14 w-full bg-[#FAF8F0] py-12 px-8 flex-center'>
-        
+    <footer className='mt-14 w-full bg-[#FAF8F0] py-12 px-8 flex-center flex-wrap max-md:gap-8 md:gap-16'>
+    <MyButton />
+    <MyButton />
     </footer>
   )
 }
