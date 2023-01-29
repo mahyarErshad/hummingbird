ebedcd14dc36975194f848384a7363ebd6484adb
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-29 09:20:31 +0330

    Created the wrapper

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='mt-14 w-full bg-[#FAF8F0] py-12 px-8 flex-center'>
+        
+    </footer>
   )
 }
 
