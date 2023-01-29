6b11dea4ed08a68af1ef0296632c5054d47f34b9
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-29 09:41:57 +0330

    Buttons are redirecting to the destination

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import MyButton from '../Utils/MyButton/MyButton'
+import React from "react";
+import MyButton from "../Utils/MyButton/MyButton";
 
 function Footer() {
   return (
-    <footer className='mt-14 w-full bg-[#FAF8F0] py-12 px-8 flex-center flex-wrap max-md:gap-8 md:gap-16'>
-    <MyButton />
-    <MyButton />
+    <footer className="mt-14 w-full bg-[#FAF8F0] py-12 px-8 flex-center flex-wrap max-md:gap-8 md:gap-16">
+      <MyButton onClick={() => window.open("https://www.zhaket.com/web/hummingbird-pro/support", "_blank")} className="max-sm:w-full" text="ارتباط با پشتیبانی" />
+      <MyButton onClick={() => window.open("https://www.zhaket.com/web/hummingbird-pro", "_blank")} className="max-sm:w-full" text="خرید نسخه حرفه ای" />
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/Components/Utils/MyButton/MyButton.jsx
+++ src/Components/Utils/MyButton/MyButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function MyButton({ text, onClick, className }) {
   return (
-    <button className={`${className}`} onClick={() => onClick}>
+    <button className={`${className}`} onClick={onClick}>
       {text}
     </button>
   );
