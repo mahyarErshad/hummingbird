897e101bc5c9647deb1d837b61cf36eafcd1099e
Mahyar Ershad <mahyar.ershad92@gmail.com>
2023-01-30 08:38:55 +0330

    Shadow is lg on hover

--- src/Components/Utils/MyButton/MyButton.jsx
+++ src/Components/Utils/MyButton/MyButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function MyButton({ text, onClick, className }) {
   return (
-    <button className={`transition-all duration-300 border rounded-lg hover:shadow-md active:shadow-sm py-4 max-sm:w-full px-8 hover:opacity-90 text-white ${className}`} onClick={onClick}>
+    <button className={`transition-all duration-300 border rounded-lg hover:shadow-lg active:shadow-sm py-4 max-sm:w-full px-8 hover:opacity-90 text-white ${className}`} onClick={onClick}>
       {text}
     </button>
   );
